2a6dc364eb9be33c77ec7aaf5f30939f7e1241b8
tahirdotz <tahirzaman075@gmail.com>
2025-04-14 02:00:23 +0600

    Added something new in case console doesnt get cleared

--- untitled/src/TerminalUtilForGuessTheNumber.java
+++ untitled/src/TerminalUtilForGuessTheNumber.java
@@ -2,15 +2,20 @@ import java.io.IOException;
 
 public class TerminalUtilForGuessTheNumber {
 
-        public static void clearTerminal() throws IOException, InterruptedException {
+    public static void clearTerminal() {
+        try {
             if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             } else {
                 System.out.print("\033[H\033[2J");
                 System.out.flush();
             }
+        } catch (IOException | InterruptedException e) {
+            for (int i = 0; i < 20; i++) {
+                System.out.println();
+            }
         }
+    }
     }
 
 
