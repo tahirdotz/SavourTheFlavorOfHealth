b0be223fa00b477ea1dfb98f3bfbfe01b812d699
tahirdotz <tahirzaman075@gmail.com>
2025-04-13 21:37:18 +0600

    Pro zombie added with better features

---
+++ untitled/src/ProZombie.java
@@ -0,0 +1,21 @@
+class ProZombie implements HireableZombie {
+    @Override
+    public String getName() {
+        return "Pro Zombie Scout";
+    }
+
+    @Override
+    public int getHireCost() {
+        return 40;
+    }
+
+    @Override
+    public double getAccuracy() {
+        return 0.9; 
+    }
+
+    @Override
+    public Prey locatePrey(PreyFactory factory) {
+        return factory.createBetterPrey(50, 100);
+    }
+}
